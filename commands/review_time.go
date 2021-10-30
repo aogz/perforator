212b9057ff118891b7ffdfd2d456f83eb0e9e04b
Sasha Ogurtov <sasha@surfly.com>
2021-10-30 14:33:18 +0200

    add tests for review time

--- commands/review_time.go
+++ commands/review_time.go
@@ -5,9 +5,10 @@ import (
 	"time"
 
 	"github.com/aogz/perforator/gh"
+	"github.com/google/go-github/v39/github"
 )
 
-// ReviewTime shows percentage of rejected PRs
+// ReviewTime shows average review time per pr author or reviewer
 func ReviewTime(owner string, repo string, limit int, groupBy string) {
 	client := gh.GetClient()
 	prs, err := gh.GetPRs(client, owner, repo, limit)
@@ -17,52 +18,86 @@ func ReviewTime(owner string, repo string, limit int, groupBy string) {
 	}
 
 	stats := map[string][]time.Duration{}
+	if groupBy == "author" {
+		stats = calculateReviewTimeByAuthor(stats, prs)
+	} else {
+		stats = calculateReviewTimeByReviewer(stats, prs, client)
+	}
+
+	calculateAggregatedResultsPerUser(stats)
+}
+
+func calculateReviewTimeByAuthor(stats map[string][]time.Duration, prs []*github.PullRequest) map[string][]time.Duration {
 	for i, pr := range prs {
 		username := *pr.User.Login
-		prNumber := *pr.Number
-		createdAt := *pr.CreatedAt
-		fmt.Printf("%d/%d Processing PR #%d created by %s\n", i+1, limit, prNumber, username)
+		fmt.Printf("%d Processing PR #%d created by %s\n", i+1, *pr.Number, username)
 
-		if groupBy == "author" {
-			if pr.MergedAt != nil {
-				mergedAt := *pr.MergedAt
-				inReviewTime := mergedAt.Sub(createdAt)
-				stats[username] = append(stats[username], inReviewTime)
-			}
-		} else {
-			reviews, err := gh.GetPullRequestReviews(client, owner, repo, prNumber)
-			if err != nil {
-				fmt.Println(err.Error())
-				return
-			}
-
-			for _, review := range reviews {
-				reviewedBy := *review.User.Login
-				reviewedAt := *review.SubmittedAt
-				inReviewTime := reviewedAt.Sub(createdAt)
-				stats[reviewedBy] = append(stats[reviewedBy], inReviewTime)
-			}
+		inReviewTime := calculatePRInReviewTime(pr)
+		if inReviewTime > 0 {
+			stats[username] = append(stats[username], inReviewTime)
 		}
 	}
 
-	getResults(stats)
+	return stats
 }
 
-func getResults(stats map[string][]time.Duration) {
+func calculateReviewTimeByReviewer(stats map[string][]time.Duration, prs []*github.PullRequest, client *github.Client) map[string][]time.Duration {
+	for i, pr := range prs {
+		fmt.Printf("%d Processing PR #%d created by %s\n", i+1, *pr.Number, *pr.User.Login)
+		reviews, err := gh.GetPullRequestReviews(client, *pr.Base.Repo.Owner.Login, *pr.Base.Repo.Name, *pr.Number)
+		if err != nil {
+			fmt.Println(err.Error())
+			return stats
+		}
+
+		for _, review := range reviews {
+			inReviewTime := calculateUserReviewTime(pr, review)
+			stats[*review.User.Login] = append(stats[*review.User.Login], inReviewTime)
+		}
+	}
+
+	return stats
+}
+
+func calculateAggregatedResultsPerUser(stats map[string][]time.Duration) float64 {
 	fmt.Println("-----")
 	var sumAggregatedDurationInHours float64
 	for username, durations := range stats {
-		var durationSum time.Duration
-		for _, duration := range durations {
-			durationSum += duration
-		}
-
-		aggregatedDuration := durationSum.Seconds() / float64(len(durations))
-		aggregatedDurationInHours := aggregatedDuration / 60 / 60
-		sumAggregatedDurationInHours += aggregatedDurationInHours
-		fmt.Printf("@%s's average review time is: %.2f hours\n", username, aggregatedDurationInHours)
+		averageReviewTime := calculateAverageReviewTimePerUser(durations)
+		sumAggregatedDurationInHours += averageReviewTime
+		fmt.Printf("@%s's average review time is: %.2f hours\n", username, averageReviewTime)
 	}
-	averageAggregatedDurationInHours := sumAggregatedDurationInHours / float64(len(stats))
+	averageAggregatedDurationInHours := calculateAggregatedAverageReviewTime(sumAggregatedDurationInHours, len(stats))
 	fmt.Printf("\nAggregated PR review duration is: %.2f hours (From %d Devs)\n", averageAggregatedDurationInHours, len(stats))
+	return averageAggregatedDurationInHours
+}
 
+func calculateUserReviewTime(pr *github.PullRequest, review *github.PullRequestReview) time.Duration {
+	reviewedAt := *review.SubmittedAt
+	return reviewedAt.Sub(*pr.CreatedAt)
+}
+
+func calculatePRInReviewTime(pr *github.PullRequest) time.Duration {
+	var inReviewTime time.Duration
+	createdAt := *pr.CreatedAt
+	if pr.MergedAt != nil {
+		mergedAt := *pr.MergedAt
+		inReviewTime = mergedAt.Sub(createdAt)
+	}
+
+	return inReviewTime
+}
+
+// calculateAverageReviewTimePerUser returns average time in seconds
+func calculateAverageReviewTimePerUser(durations []time.Duration) float64 {
+	var durationSum time.Duration
+	for _, duration := range durations {
+		durationSum += duration
+	}
+
+	return durationSum.Hours() / float64(len(durations))
+}
+
+func calculateAggregatedAverageReviewTime(sumAggregatedDurationInHours float64, count int) float64 {
+	return sumAggregatedDurationInHours / float64(count)
 }
---
+++ commands/review_time_test.go
@@ -0,0 +1,159 @@
+package commands
+
+import (
+	"testing"
+	"time"
+
+	"github.com/google/go-github/v39/github"
+)
+
+func TestCalculateReviewTimeByAuthor(t *testing.T) {
+	now := time.Now()
+	in6Hours := now.Add(time.Hour * 6)
+	in12Hours := now.Add(time.Hour * 12)
+	tomorrow := now.Add(time.Hour * 24)
+	username1 := "Foo"
+	username2 := "Bar"
+	prId := 1
+	prs := []*github.PullRequest{
+		{
+			User: &github.User{
+				Login: &username1,
+			},
+			Number:    &prId,
+			CreatedAt: &now,
+			MergedAt:  &in6Hours,
+		},
+		{
+			User: &github.User{
+				Login: &username1,
+			},
+			Number:    &prId,
+			CreatedAt: &now,
+			MergedAt:  &tomorrow,
+		},
+		{
+			User: &github.User{
+				Login: &username2,
+			},
+			Number:    &prId,
+			CreatedAt: &now,
+			MergedAt:  &in12Hours,
+		},
+		{
+			User: &github.User{
+				Login: &username2,
+			},
+			Number:    &prId,
+			CreatedAt: &now,
+			MergedAt:  nil,
+		},
+	}
+
+	initial := map[string][]time.Duration{}
+	result := calculateReviewTimeByAuthor(initial, prs)
+	if len(result) != 2 {
+		t.Fatalf("Unexpected result length, expected 2 got %d", len(result))
+	}
+	if len(result[username1]) != 2 {
+		t.Fatalf("Unexpected len of results for user1, expected 2 got %d", len(result[username1]))
+	}
+	if result[username1][0] != time.Hour*6 && result[username1][1] != time.Hour*24 {
+		t.Fatalf("Unexpected results for user1, expected 6 and 24 got %d %d", result[username1][0], result[username1][1])
+	}
+	if len(result[username2]) != 1 {
+		t.Fatalf("Unexpected len of results for user1, expected 2 got %d", len(result[username1]))
+	}
+	if result[username2][0] != time.Hour*12 {
+		t.Fatalf("Unexpected results for user1, expected 12 got %d", result[username2][0])
+	}
+}
+
+func TestGetResults(t *testing.T) {
+	stats := map[string][]time.Duration{
+		"foo": {time.Minute * 1, time.Minute * 2, time.Minute * 3},
+		"bar": {time.Minute * 4, time.Minute * 5, time.Minute * 6},
+		"baz": {time.Minute * 7, time.Minute * 8, time.Minute * 9},
+	}
+
+	results := calculateAggregatedResultsPerUser(stats)
+	expected := 5.0 / 60.0
+	if results != float64(expected) {
+		t.Fatalf("Invalid aggregated results, expected %f got %f", expected, results)
+	}
+}
+
+func TestCalculateUserReviewTime(t *testing.T) {
+	now := time.Now()
+	tomorrow := now.Add(time.Hour * 24)
+
+	pr := github.PullRequest{
+		CreatedAt: &now,
+	}
+
+	review := github.PullRequestReview{
+		SubmittedAt: &tomorrow,
+	}
+
+	reviewTime := calculateUserReviewTime(&pr, &review)
+	if reviewTime != time.Hour*24 {
+		t.Fatalf("Invalid time in review, expected 24h got %s", reviewTime)
+	}
+}
+
+func TestCalculatePRInReviewTime(t *testing.T) {
+	now := time.Now()
+	tomorrow := now.Add(time.Hour * 24)
+
+	pr := github.PullRequest{
+		CreatedAt: &now,
+		MergedAt:  &tomorrow,
+	}
+
+	timeInReview := calculatePRInReviewTime(&pr)
+	if timeInReview != time.Hour*24 {
+		t.Fatalf("Invalid time in review, expected 24h got %s", timeInReview)
+	}
+}
+
+func TestCalculatePRInReviewTimeNotMerged(t *testing.T) {
+	now := time.Now()
+
+	pr := github.PullRequest{
+		CreatedAt: &now,
+		MergedAt:  nil,
+	}
+
+	timeInReview := calculatePRInReviewTime(&pr)
+	if timeInReview != 0 {
+		t.Fatalf("Invalid time in review, expected 0 got %s", timeInReview)
+	}
+}
+
+func TestCalculateAverageReviewTimePerUser(t *testing.T) {
+	durations := []time.Duration{
+		time.Minute * 1,
+		time.Minute * 2,
+		time.Minute * 3,
+	}
+	averageReviewTime := calculateAverageReviewTimePerUser(durations)
+	expected := 120.0 / 60 / 60
+	if averageReviewTime != expected {
+		t.Fatalf("Invalid averageReviewTime, expected %f got %f", expected, averageReviewTime)
+	}
+
+}
+
+func TestCalculateAggregatedAverageReviewTime(t *testing.T) {
+	if calculateAggregatedAverageReviewTime(100.0, 10) != 10.0 {
+		t.Fatal("Invalid result")
+	}
+
+	if calculateAggregatedAverageReviewTime(24.0, 10) != 2.4 {
+		t.Fatal("Invalid result")
+	}
+
+	if calculateAggregatedAverageReviewTime(15.0, 10) != 1.5 {
+		t.Fatal("Invalid result")
+	}
+}
