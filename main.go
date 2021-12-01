fce64467232fa9e9818fad57a6c4ee0e81a197a9
Sasha Ogurtov <sasha@surfly.com>
2021-12-02 00:06:50 +0100

    better help text

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 
 	"github.com/aogz/perforator/commands"
@@ -23,39 +22,23 @@ func addDefaultArgs(subcommand *flag.FlagSet) (*string, *int) {
 }
 
 func main() {
-	var repo *string
-	var limit *int
-
-	helpCmd := flag.Bool("help", false, "Help text")
-	flag.Parse()
-
-	prRejectionRateCmd := flag.NewFlagSet(rejectionRate, flag.ExitOnError)
-	prReviewTimeCmd := flag.NewFlagSet(reviewTime, flag.ExitOnError)
-
-	if *helpCmd {
-		fmt.Println("Usage: `$ perforator [command] [--help] [flags]`")
-		fmt.Printf("Commands: %s, %s\n", rejectionRate, reviewTime)
-		os.Exit(0)
-	}
-
-	if len(os.Args) < 2 {
-		fmt.Println("Invalid format. Please use: `$ perforator --help`")
-		os.Exit(1)
-	}
+	utils.AddHelp()
+	utils.ValidateArgs()
 
 	command := os.Args[1]
 	switch command {
 	case rejectionRate:
-		repo, limit = addDefaultArgs(prRejectionRateCmd)
+		prRejectionRateCmd := flag.NewFlagSet(rejectionRate, flag.ExitOnError)
+		repo, limit := addDefaultArgs(prRejectionRateCmd)
 		owner, repoName := utils.ParseRepo(*repo)
 		commands.RejectionRate(owner, repoName, *limit)
 	case reviewTime:
+		prReviewTimeCmd := flag.NewFlagSet(reviewTime, flag.ExitOnError)
 		groupBy := prReviewTimeCmd.String("group-by", "reviewer", "Criteria to group by. Accepted values: author or reviewer")
-		repo, limit = addDefaultArgs(prReviewTimeCmd)
+		repo, limit := addDefaultArgs(prReviewTimeCmd)
 		owner, repoName := utils.ParseRepo(*repo)
 		commands.ReviewTime(owner, repoName, *limit, *groupBy)
 	default:
-		fmt.Println("Invalid command")
-		fmt.Println("Use `$ perforator --help` to get more help")
+		utils.PrintHelp()
 	}
 }
---
+++ utils/help.go
@@ -0,0 +1,40 @@
+package utils
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+const HELP_TEXT = `Perforator is a CLI tool to track and analyze performance and statistics based on the information from GitHub Pull Requests.
+
+Usage:
+	perforator [command] [flags]
+
+Commands:
+	rejection-rate	Calculate the aggregated rejection rate of pull requests grouped by contibutor.
+	review-time	Calculate the aggregated review time of pull requests grouped by author/reviewer.
+
+Flags:
+	--help: Prints help information.
+`
+
+func PrintHelp() {
+	fmt.Println(HELP_TEXT)
+	os.Exit(0)
+}
+
+func AddHelp() {
+	helpCmd := flag.Bool("help", false, "Prints help information")
+	hCmd := flag.Bool("h", false, "Prints help information")
+	flag.Parse()
+	if *helpCmd || *hCmd {
+		PrintHelp()
+	}
+}
+
+func ValidateArgs() {
+	if len(os.Args) < 2 {
+		PrintHelp()
+	}
+}
